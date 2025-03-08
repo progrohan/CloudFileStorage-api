d76371a3c39c27fc020ea8501d3e286c6843ab3b
Andrey <aprishlov@gmail.com>
2025-03-08 23:56:58 +0300

    feat: created securityFilterChain method

--- src/main/java/com/progrohan/cloud_file_storage/config/SecurityConfig.java
+++ src/main/java/com/progrohan/cloud_file_storage/config/SecurityConfig.java
@@ -1,14 +1,29 @@
 package com.progrohan.cloud_file_storage.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/sign-up").permitAll()
+                        .requestMatchers("/api/sign-in").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder(){
