cfcb07b6201fb943a37e30f65e7451e0ec6738cc
Andrey <aprishlov@gmail.com>
2025-03-11 22:48:07 +0300

    feat: create UserExistException

---
+++ src/main/java/com/progrohan/cloud_file_storage/exception/UserExistException.java
@@ -0,0 +1,9 @@
+package com.progrohan.cloud_file_storage.exception;
+
+public class UserExistException extends RuntimeException{
+
+    public UserExistException(String msg){
+        super(msg);
+    }
+
+}
