472b3cde564670704ef9b0ba7e028f4e37002bb4
Andrey <aprishlov@gmail.com>
2025-03-11 22:47:14 +0300

    chore: rename user field to username

--- src/main/java/com/progrohan/cloud_file_storage/dto/UserRequestDTO.java
+++ src/main/java/com/progrohan/cloud_file_storage/dto/UserRequestDTO.java
@@ -1,17 +1,15 @@
 package com.progrohan.cloud_file_storage.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRequestDTO {
 
-    private String name;
+    private String username;
 
     private String password;
 
--- src/main/java/com/progrohan/cloud_file_storage/dto/UserResponseDTO.java
+++ src/main/java/com/progrohan/cloud_file_storage/dto/UserResponseDTO.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserResponseDTO {
 
-    private String name;
+    private String username;
 
 }
