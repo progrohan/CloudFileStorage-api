a69a9b64c52553b735561c8ae75a06a98684860b
Andrey <aprishlov@gmail.com>
2025-03-11 22:49:45 +0300

    feat: create UnknownExceptionHandler

---
+++ src/main/java/com/progrohan/cloud_file_storage/handler/UnknownExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.progrohan.cloud_file_storage.handler;
+
+import com.progrohan.cloud_file_storage.dto.ErrorResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class UnknownExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleException(Exception e) {
+        return ResponseEntity.internalServerError().body(new ErrorResponseDTO("Unknown error"));
+    }
+
+}
