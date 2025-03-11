7832dc2c9371f7accdbf39c2bdb5de1fe7946a62
Andrey <aprishlov@gmail.com>
2025-03-11 22:53:21 +0300

    feat: create signIn method

--- src/main/java/com/progrohan/cloud_file_storage/controller/AuthController.java
+++ src/main/java/com/progrohan/cloud_file_storage/controller/AuthController.java
@@ -1,28 +1,29 @@
 package com.progrohan.cloud_file_storage.controller;
 
-import com.progrohan.cloud_file_storage.dto.ResponseMessageDTO;
-import com.progrohan.cloud_file_storage.dto.UserRegistrationDTO;
 import com.progrohan.cloud_file_storage.dto.UserRequestDTO;
 import com.progrohan.cloud_file_storage.dto.UserResponseDTO;
 import com.progrohan.cloud_file_storage.service.AuthService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
 @RestController
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<UserResponseDTO> signUp(@RequestBody UserRegistrationDTO userRegistrationDTO){
+    public ResponseEntity<UserResponseDTO> signUp(@RequestBody UserRequestDTO userRequestDTO){
 
-        UserResponseDTO user = authService.createUser(userRegistrationDTO);
+        UserResponseDTO user = authService.createUser(userRequestDTO);
 
         return ResponseEntity
                 .created(URI.create("api/user/me"))
@@ -30,8 +31,12 @@ public class AuthController {
 
     }
 
-    public ResponseEntity<UserResponseDTO> signIn(@RequestBody UserRequestDTO userRequestDTO){
+    @PostMapping("/sign-in")
+    public ResponseEntity<UserResponseDTO> signIn(@RequestBody UserRequestDTO userRequestDTO, HttpSession session){
 
+        UserResponseDTO user = authService.loginUser(userRequestDTO, session);
+
+        return ResponseEntity.ok(user);
 
     }
 
