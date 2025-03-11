eba9f197fd7cb52161c7a19754188aa940e3e1f7
Andrey <aprishlov@gmail.com>
2025-03-11 22:53:56 +0300

    feat: create loginUser method

--- src/main/java/com/progrohan/cloud_file_storage/service/AuthService.java
+++ src/main/java/com/progrohan/cloud_file_storage/service/AuthService.java
@@ -1,11 +1,20 @@
 package com.progrohan.cloud_file_storage.service;
 
-import com.progrohan.cloud_file_storage.dto.UserRegistrationDTO;
+import com.progrohan.cloud_file_storage.dto.UserRequestDTO;
 import com.progrohan.cloud_file_storage.dto.UserResponseDTO;
 import com.progrohan.cloud_file_storage.entity.UserEntity;
+import com.progrohan.cloud_file_storage.exception.AuthException;
+import com.progrohan.cloud_file_storage.exception.UserExistException;
 import com.progrohan.cloud_file_storage.mapper.UserMapper;
 import com.progrohan.cloud_file_storage.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,19 +22,38 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthService {
 
+    private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
-    public UserResponseDTO createUser(UserRegistrationDTO userRegistrationDTO){
+    public UserResponseDTO createUser(UserRequestDTO userRequestDTO){
 
-        userRegistrationDTO.setPassword(passwordEncoder
-                .encode(userRegistrationDTO.getPassword()));
+        if(userRepository.findByUsername(userRequestDTO.getUsername()).isPresent())
+            throw new UserExistException("User with username " + userRequestDTO.getUsername() + " already exists!");
 
-        UserEntity userEntity = userRepository.saveAndFlush(userMapper.toEntity(userRegistrationDTO));
+        userRequestDTO.setPassword(passwordEncoder
+                .encode(userRequestDTO.getPassword()));
+
+        UserEntity userEntity = userRepository.saveAndFlush(userMapper.toEntity(userRequestDTO));
 
         return userMapper.toDto(userEntity);
 
+    }
+
+    public UserResponseDTO loginUser(UserRequestDTO user, HttpSession session){
+        try{
+            Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(auth);
+
+            session.setAttribute("SPRING_SECURITY_CONTEXT", context);
+
+            return  userMapper.toResponseDto(user);
+        }catch (AuthenticationException e){
+            throw new AuthException("Credentials incorrect!");
+        }
     }
 
 }
