a79f0611f66ef3cf96765476bff7df0214da8b34
Andrey <aprishlov@gmail.com>
2025-03-11 22:52:36 +0300

    chore: changed mapper, cause UserRegistrationDTO was deleted

--- src/main/java/com/progrohan/cloud_file_storage/mapper/UserMapper.java
+++ src/main/java/com/progrohan/cloud_file_storage/mapper/UserMapper.java
@@ -1,6 +1,6 @@
 package com.progrohan.cloud_file_storage.mapper;
 
-import com.progrohan.cloud_file_storage.dto.UserRegistrationDTO;
+import com.progrohan.cloud_file_storage.dto.UserRequestDTO;
 import com.progrohan.cloud_file_storage.dto.UserResponseDTO;
 import com.progrohan.cloud_file_storage.entity.UserEntity;
 import org.mapstruct.Mapper;
@@ -8,8 +8,10 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    UserEntity toEntity(UserRegistrationDTO dto);
+    UserEntity toEntity(UserRequestDTO dto);
 
     UserResponseDTO toDto(UserEntity entity);
+
+    UserResponseDTO toResponseDto(UserRequestDTO requestDTO);
 
 }
