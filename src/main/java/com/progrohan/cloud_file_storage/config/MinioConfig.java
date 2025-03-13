4c76c1cbebb9980843f272833427caa2fbab1679
Andrey <aprishlov@gmail.com>
2025-03-13 22:29:05 +0300

    feat: created MinioConfig

---
+++ src/main/java/com/progrohan/cloud_file_storage/config/MinioConfig.java
@@ -0,0 +1,30 @@
+package com.progrohan.cloud_file_storage.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.url}")
+    private String minioUrl;
+
+    @Value("${minio.accessKey}")
+    private String accessKey;
+
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioUrl)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+
+}
