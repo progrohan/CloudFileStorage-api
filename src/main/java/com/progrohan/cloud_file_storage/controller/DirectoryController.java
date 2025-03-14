4fdb1c6fc6f810396274f0cc8c2b0abeaa45f8c8
Andrey <aprishlov@gmail.com>
2025-03-14 23:53:35 +0300

    feat: create DirectoryController

---
+++ src/main/java/com/progrohan/cloud_file_storage/controller/DirectoryController.java
@@ -0,0 +1,35 @@
+package com.progrohan.cloud_file_storage.controller;
+
+import com.progrohan.cloud_file_storage.dto.ResourceResponseDTO;
+import com.progrohan.cloud_file_storage.service.DirectoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+
+@RestController
+@RequestMapping("/api/directory")
+@RequiredArgsConstructor
+public class DirectoryController {
+
+    private final DirectoryService directoryService;
+
+    @PostMapping()
+    public ResponseEntity<ResourceResponseDTO> createEmptyDirectory(@RequestParam String path,
+                                                                    @AuthenticationPrincipal UserDetails userDetails){
+        ResourceResponseDTO resource = directoryService.createEmptyDirectory(userDetails.getUsername(), path);
+
+
+        return ResponseEntity.created(URI.create("/api/directory")).body(resource);
+    }
+
+
+}
