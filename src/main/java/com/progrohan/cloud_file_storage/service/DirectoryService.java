4730b457e7040602f9a0911281edfa737d5e1e54
Andrey <aprishlov@gmail.com>
2025-03-15 23:33:09 +0300

    feat: create getDirectoriesResources method

--- src/main/java/com/progrohan/cloud_file_storage/service/DirectoryService.java
+++ src/main/java/com/progrohan/cloud_file_storage/service/DirectoryService.java
@@ -2,15 +2,21 @@ package com.progrohan.cloud_file_storage.service;
 
 
 import com.progrohan.cloud_file_storage.dto.ResourceResponseDTO;
+import com.progrohan.cloud_file_storage.exception.StorageException;
 import com.progrohan.cloud_file_storage.repository.MinioStorageRepository;
+import io.minio.Result;
+import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class DirectoryService {
 
-    private final UserService userService;
     private final MinioStorageRepository storageRepository;
     private final ResourceService resourceService;
 
@@ -18,9 +24,37 @@ public class DirectoryService {
 
         String finalPath = storageRepository.getUserRootFolderByName(userName) + path + "/";
 
-        storageRepository.createEmptyDirectory(userName, finalPath);
+        storageRepository.createEmptyDirectory(finalPath);
 
-        return resourceService.getResource(finalPath);
+        return resourceService.getResource(userName, path);
+
+    }
+
+    public List<ResourceResponseDTO> getDirectoriesResources(String userName, String path){
+
+        List<ResourceResponseDTO> resources = new ArrayList<>();
+
+        String finalPath = storageRepository.getUserRootFolderByName(userName) + path + "/";
+
+        try {
+            Iterable<Result<Item>> results = storageRepository.getDirectoriesResources(finalPath);
+
+            for (Result<Item> result : results) {
+                Item item = result.get();
+                String resourcePath = item.objectName();
+
+                if (Objects.equals(resourcePath, finalPath )) continue;
+
+                int firstSlashIndex = resourcePath.indexOf('/');
+                int lastSlashIndex = resourcePath.lastIndexOf('/');
+
+                resources.add(resourceService.getResource(userName, resourcePath.substring(firstSlashIndex + 1, lastSlashIndex)));
+            }
+
+            return resources;
+        }catch (Exception e){
+            throw new StorageException("Problem with getting directories resources!");
+        }
 
     }
 
