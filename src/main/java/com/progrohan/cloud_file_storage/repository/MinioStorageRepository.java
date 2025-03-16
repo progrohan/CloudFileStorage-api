b09d2dca6e33d317aa20d31feada6a7ff5ee6d3e
Andrey <aprishlov@gmail.com>
2025-03-16 23:41:20 +0300

    feat: create deleteResource method

--- src/main/java/com/progrohan/cloud_file_storage/repository/MinioStorageRepository.java
+++ src/main/java/com/progrohan/cloud_file_storage/repository/MinioStorageRepository.java
@@ -1,15 +1,20 @@
 package com.progrohan.cloud_file_storage.repository;
 
 
+import com.progrohan.cloud_file_storage.exception.ResourceNotFoundException;
 import com.progrohan.cloud_file_storage.exception.StorageException;
 import com.progrohan.cloud_file_storage.service.UserService;
 import io.minio.BucketExistsArgs;
+import io.minio.CopyObjectArgs;
+import io.minio.CopySource;
 import io.minio.ListObjectsArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
 import io.minio.Result;
 import io.minio.StatObjectArgs;
+import io.minio.errors.MinioException;
 import io.minio.messages.Item;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +22,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayInputStream;
+import java.util.Iterator;
 
 @Service
 @RequiredArgsConstructor
@@ -102,5 +108,67 @@ public class MinioStorageRepository {
     }
 
 
+    public void deleteResource(String path){
+        try{
+            minioClient.removeObject(RemoveObjectArgs.builder()
+                            .bucket(rootBucket)
+                            .object(path)
+                            .build());
+        }catch(Exception e){
+            throw new StorageException("Problem with deleting resource");
+        }
+    }
+
+    public void checkIfResourceExists(String path){
+        if (!path.endsWith("/")){
+            try{
+                minioClient.statObject(StatObjectArgs.builder()
+                        .bucket(rootBucket)
+                        .object(path)
+                        .build());
+            }catch (MinioException e){
+                throw new ResourceNotFoundException("Resource not found!");
+            }catch (Exception e){
+                throw new StorageException("Problem with getting resource!");
+            }
+        }
+    }
+
+    public void renameFile(String path, String newPath){
+        try{
+            minioClient.copyObject(CopyObjectArgs.builder()
+                    .source(CopySource.builder()
+                            .bucket(rootBucket)
+                            .object(path)
+                            .build())
+                    .bucket(rootBucket)
+                    .object(newPath)
+                    .build());
+        }catch (Exception e){
+            throw new StorageException("Problem with renaming file!");
+        }
+        deleteResource(path);
+    }
+
+    public void renameFolder(String path, String newPath){
+
+        try{
+            Iterable<Result<Item>> results = minioClient.listObjects(
+                    ListObjectsArgs.builder().bucket(rootBucket).prefix(path).recursive(true).build()
+            );
+
+            for (Result<Item> result : results) {
+                Item item = result.get();
+                String oldObjectName = item.objectName();
+                String newObjectName = oldObjectName.replaceFirst(path, newPath);
+
+                renameFile(oldObjectName, newObjectName);
+
+            }
+        }catch (Exception e){
+            throw new StorageException("Problem with renaming folder!");
+        }
+
+    }
 
 }
