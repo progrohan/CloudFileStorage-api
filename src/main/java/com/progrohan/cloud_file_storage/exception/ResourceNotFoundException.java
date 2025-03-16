ac7027e5fdbc558f1695bc6fa156c137ca50be38
Andrey <aprishlov@gmail.com>
2025-03-16 23:40:32 +0300

    feat: create ResourceNotFoundException

---
+++ src/main/java/com/progrohan/cloud_file_storage/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.progrohan.cloud_file_storage.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String msg){
+        super(msg);
+    }
+
+}
