2a3934205b61e8f27c0fe40e5da55bb8a504f2c6
Andrey <aprishlov@gmail.com>
2025-03-16 23:43:02 +0300

    feat: create move and delete methods

--- src/main/java/com/progrohan/cloud_file_storage/controller/ResourceController.java
+++ src/main/java/com/progrohan/cloud_file_storage/controller/ResourceController.java
@@ -3,12 +3,15 @@ package com.progrohan.cloud_file_storage.controller;
 import com.progrohan.cloud_file_storage.dto.ResourceResponseDTO;
 import com.progrohan.cloud_file_storage.service.ResourceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,6 +27,23 @@ public class ResourceController {
         ResourceResponseDTO resource = resourceService.getResource(userDetails.getUsername(), path);
 
         return ResponseEntity.ok(resource);
+
+    }
+
+    @GetMapping("/move")
+    public ResponseEntity<ResourceResponseDTO> moveResource(@AuthenticationPrincipal UserDetails userDetails, @RequestParam String from, @RequestParam String to){
+
+        ResourceResponseDTO resource = resourceService.renameResource(userDetails.getUsername(), from, to);
+
+        return ResponseEntity.ok(resource);
+
+    }
+
+    @DeleteMapping()
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteResource(@AuthenticationPrincipal UserDetails userDetails, @RequestParam String path){
+
+        resourceService.deleteResource(userDetails.getUsername(), path);
 
     }
 
