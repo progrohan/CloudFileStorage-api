ffd866562ca82bfea65ef5e81f4d2b0f9137d4bc
Andrey <aprishlov@gmail.com>
2025-03-16 23:40:46 +0300

    feat: create ResourceNotFoundExceptionHandler

---
+++ src/main/java/com/progrohan/cloud_file_storage/handler/ResourceNotFoundExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.progrohan.cloud_file_storage.handler;
+
+import com.progrohan.cloud_file_storage.exception.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ResourceNotFoundExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Void> handleResourceNotFoundException(ResourceNotFoundException e){
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+}
