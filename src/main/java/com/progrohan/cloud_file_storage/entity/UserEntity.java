47c0cac6637b450b2fa0e9728435b49e5d5bdf25
Andrey <aprishlov@gmail.com>
2025-12-23 15:56:05 +0300

    feat: add user_storage relation

--- src/main/java/com/progrohan/cloud_file_storage/entity/UserEntity.java
+++ src/main/java/com/progrohan/cloud_file_storage/entity/UserEntity.java
@@ -1,13 +1,19 @@
 package com.progrohan.cloud_file_storage.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -20,8 +26,12 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "username", unique = true, nullable = false)
     private String username;
 
+    @Column(name = "password", nullable = false)
     private String password;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<UserStorageEntity> storageAccesses = new HashSet<>();
 }
